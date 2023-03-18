9efe2377ca32fb65a24b4fb424af016c4e422cb2
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-03-18 17:47:20 +0100

    Edited context

--- src/Context/context.ts
+++ src/Context/context.ts
@@ -1,7 +1,18 @@
-import React from "react";
+import { EtherscanType } from './../Interfaces/EtherscanInterface';
+import React, { useState } from "react";
 
-export const Etherscan = React.createContext<string | undefined>(undefined);
+export const EtherscanContext = React.createContext<EtherscanType | undefined>(
+  undefined
+);
 
-export const EtherscanContext = ({children: }) => {
+const EtherscanProvider: React.FC<React.ReactNode> = ({ children }) => {
+  const [address, setAddress] = useState<string>("");
 
-}
+  return (
+    <EtherscanContext.Provider value={{ address, setAddress }}>
+      {children}
+    </EtherscanContext.Provider>
+  );
+};
+
+export default EtherscanProvider;
